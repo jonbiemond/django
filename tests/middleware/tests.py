38477646e58d69f79167a32c6a81ab52910dd960
Jonathan Biemond <jonbiemond@gmail.com>
2026-07-17 12:35:32 -0700

    Excluded HTTP QUERY method requests from being modified by APPEND_SLASH.
    A redirect will convert the request into a GET, dropping any request
    body. Added the QUERY method to the other methods with a body, that
    raise an error with a warning instead of redirecting.

--- tests/middleware/tests.py
+++ tests/middleware/tests.py
@@ -114,8 +114,8 @@ class CommonMiddlewareTest(SimpleTestCase):
     def test_append_slash_no_redirect_in_DEBUG(self):
         """
         While in debug mode, an exception is raised with a warning
-        when a failed attempt is made to DELETE, POST, PUT, or PATCH to an URL
-        which would normally be redirected to a slashed version.
+        when a failed attempt is made to DELETE, POST, PUT, PATCH or QUERY to
+        an URL which would normally be redirected to a slashed version.
         """
         msg = "maintaining %s data. Change your form to point to testserver/slash/"
         request = self.rf.get("/slash")
@@ -131,6 +131,10 @@ class CommonMiddlewareTest(SimpleTestCase):
         with self.assertRaisesMessage(RuntimeError, msg % request.method):
             CommonMiddleware(get_response_404)(request)
         request = self.rf.delete("/slash")
+        with self.assertRaisesMessage(RuntimeError, msg % request.method):
+            CommonMiddleware(get_response_404)(request)
+        request = self.rf.get("/slash")
+        request.method = "QUERY"
         with self.assertRaisesMessage(RuntimeError, msg % request.method):
             CommonMiddleware(get_response_404)(request)
 
